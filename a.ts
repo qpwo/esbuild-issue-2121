e251a7e6cd8118ef7a5bb8544950b3a24d824f29
Luke Harold Miles <lukem@sent.com>
2022-03-21 12:19:31 -0700

    reproduce error

---
+++ a.ts
@@ -0,0 +1,2 @@
+export const five: number = 5
+if (require.main === module) console.log({five})
---
+++ b.ts
@@ -0,0 +1,5 @@
+import { five } from './a'
+
+const twenty: number = five * 4
+
+if (require.main===module) console.log({twenty})
